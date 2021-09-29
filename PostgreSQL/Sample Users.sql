f6cbb5f7f5b784e786f88cabe6cc2e9ba3580e1a
Lauren Smith <ldsmith16@liberty.edu>
2021-09-29 08:56:00 -0500

    sample users and java connection utility

---
+++ PostgreSQL/Sample Users.sql
@@ -0,0 +1,9 @@
+insert into users (username, user_password, first_name, last_name, user_type)
+	values ('laurenpena', '2108Java', 'Lauren', 'Pena', 'employee');
+
+insert into users (username, user_password, first_name, last_name, user_type)
+	values ('employee', 'employee', 'Mary', 'Jane', 'employee');
+
+insert into users (username, user_password, first_name, last_name, user_type)
+	values ('finanace', 'manager', 'Finance', 'Manager', 'finance manager');
+
--- p1-mia-lauren/src/main/java/com/revature/util/DBConnection.java
+++ p1-mia-lauren/src/main/java/com/revature/util/DBConnection.java
@@ -1,5 +1,23 @@
 package com.revature.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnection {
 
+	//database endpoint and login credentials
+	private static final String URL = "jdbc:postgresql://mia-lauren.cagsxdx1qu3s.us-east-2.rds.amazonaws.com/postgres";
+	private static final String USERNAME = "postgres";
+	private static final String PASSWORD = "2108Java";
+	
+	public Connection getConnection() throws SQLException {
+		
+		//attempt to create a connection to the database
+		Connection conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+			
+		return conn;
+		
+		
+	}
 }
