dfa3fea29b35da500f02c05a4ea294f2d5f40f06
Lauren Smith <ldsmith16@liberty.edu>
2021-09-30 10:38:07 -0500

    debug login

--- p1-mia-lauren/src/main/java/com/revature/MainDriver.java
+++ p1-mia-lauren/src/main/java/com/revature/MainDriver.java
@@ -1,5 +1,20 @@
 package com.revature;
 
+import com.revature.presentation.UserLogin;
+import com.revature.repo.UserDAO;
+import com.revature.repo.UserDAOImpl;
+import com.revature.service.AuthenticateUser;
+import com.revature.service.AuthenticateUserImpl;
+
 public class MainDriver {
+	
+	public static void main(String[] args) {
+		
+		UserDAO userDao = new UserDAOImpl();
+		AuthenticateUser auth = new AuthenticateUserImpl(userDao);
+		UserLogin login = new UserLogin(auth);
+		
+		login.display();
+	}
 
 }
--- p1-mia-lauren/src/main/java/com/revature/repo/UserDAO.java
+++ p1-mia-lauren/src/main/java/com/revature/repo/UserDAO.java
@@ -1,5 +1,8 @@
 package com.revature.repo;
 
+import com.revature.models.User;
+
 public interface UserDAO {
 
+	public User selectUserByUsername(String username);
 }
